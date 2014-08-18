cd3d18f377fdfbf54c96f88fa78a5f1ba3fb0764
Sean Dunn <sedunn@adobe.com>
2014-08-18 18:54:57 -0400

    client: Implement basic walking functionality

--- test/specs/client.js
+++ test/specs/client.js
@@ -1,7 +1,20 @@
 define(['chai-as-promised', 'lib/client', 'lib/resource'], function(chaiAsPromised, Client, Resource) {
+  var server;
+
   chai.use(chaiAsPromised);
 
-  describe.only('Client', function() {
+  beforeEach(function() {
+    server = sinon.fakeServer.create();
+    server.autoRespond = true;
+  });
+
+  afterEach(function() {
+    server.restore();
+    server = null;
+  });
+
+
+  describe('Client', function() {
     it('can be instantiated', function() {
       expect(new Client("")).to.be.an.instanceOf(Client);
     });
@@ -17,27 +30,11 @@ define(['chai-as-promised', 'lib/client', 'lib/resource'], function(chaiAsPromis
     });
 
     describe('#walk', function() {
-      var server;
-
-      beforeEach(function() {
-        server = sinon.fakeServer.create();
-        server.autoRespond = true;
-      });
-
-      afterEach(function() {
-        server.restore();
-        server = null;
-      });
-
       describe('when given an api root', function() {
         var client = new Client('/v1/foo');
 
         describe('that does not conform to the hypermedia format', function() {
-          var response = [
-            200,
-            { "Content-Type": "application/json" },
-            JSON.stringify({})
-          ];
+          var response = [200, { "Content-Type": "application/json" }, JSON.stringify({})];
 
           it('returns a rejected promise', function() {
             server.respondWith('OPTION', '/v1/foo', response);
@@ -48,21 +45,43 @@ define(['chai-as-promised', 'lib/client', 'lib/resource'], function(chaiAsPromis
         describe('that does conform to the hypermedia format', function() {
           var links = {
             _links: {
-              self: {
-                href: '/v1/foo'
-              }
+              self: { href: '/v1/foo' }
             }
           };
-          var response = [
-            200,
-            { "Content-Type": "application/json" },
-            JSON.stringify(links)
-          ];
+          var response = [200, { "Content-Type": "application/json" }, JSON.stringify(links)];
 
           it('returns a resolved promise ', function() {
             server.respondWith('OPTION', '/v1/foo', response);
-            return expect(client.walk()).to.eventually.be.an.instanceOf(Resource);
+            return expect(client.walk()).to.become(new Resource(links));
           });
+        });
+      });
+
+      describe('when given a shortname', function() {
+        var client = new Client('/v1');
+        var links = {
+          root: {
+            _links: {
+              self: { href: '/v1' },
+              foo: { href: '/v1/foo' }
+            }
+          },
+          foo: {
+            _links: {
+              self: { href: '/v1/foo' }
+            }
+          }
+        };
+        var responses = {
+          root: [200, { "Content-Type": "application/json" }, JSON.stringify(links.root)],
+          foo: [200, { "Content-Type": "application/json" }, JSON.stringify(links.foo)]
+        };
+
+        it('returns a resolved promise of a resouce', function() {
+          server.respondWith('OPTION', '/v1', responses.root);
+          server.respondWith('OPTION', '/v1/foo', responses.foo);
+
+          return expect(client.walk('foo')).to.become(new Resource(links.foo));
         });
       });
     });
